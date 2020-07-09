8641c6fb5386088e5ace677c0ab65d6098f6531d
tankcaster <tankcaster@gmail.com>
2020-07-09 03:42:47 -0500

    implementing netlify forms

--- src/components/contact.js
+++ src/components/contact.js
@@ -27,7 +27,7 @@ const Button = styled.button`
     min-width: 10rem;
     border-radius: .5rem;
     box-shadow: none;
-    background: ${props => 
+    background: ${props =>
         props.done ? 'green' : props.error ? 'red' : 'teal'
     };
     color: white;
@@ -55,16 +55,22 @@ const rotate = keyframes`
 const LoadingButton = (props => (
     <Button red={props.error} disabled={props.loading || props.done || props.error} {...props}>
         {
-            props.error 
-            ? 'Error'
-            : props.loading
-                ? 'Sending...' 
-                : props.done 
-                    ? props.formStatus || 'Done'
-                    : props.children
+            props.error
+                ? 'Error'
+                : props.loading
+                    ? 'Sending...'
+                    : props.done
+                        ? props.formStatus || 'Done'
+                        : props.children
         }
     </Button>
 ))
+
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
 
 export default class Contact extends React.Component {
 
@@ -77,68 +83,65 @@ export default class Contact extends React.Component {
 
     handleSubmit = async (info, { setSubmitting }) => {
         return new Promise((resolve, reject) => {
-            fetch(process.env.GATSBY_API_URL, {
+            fetch('/', {
                 method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(info)
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: encode({ "form-name": "contact", ...info })
             })
-            .then(res => {
-                if(res.status == 200) {
-                    this.setState({done: true, formStatus: 'Sent'});
-                    resolve();
-                } else {
-                    this.setState({error: true, formStatus: 'Error'})
-                    console.error(res);
-                    reject(res);
-                }
-            })
-            .catch(err => {
-                console.error(err);
-                this.setState({error: true, formStatus: 'Error'})
-                reject();
-            })
+                .then(res => {
+                    if (res.status == 200) {
+                        this.setState({ done: true, formStatus: 'Sent' });
+                        resolve();
+                    } else {
+                        this.setState({ error: true, formStatus: 'Error' })
+                        console.error(res);
+                        reject(res);
+                    }
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.setState({ error: true, formStatus: 'Error' })
+                    reject();
+                })
         })
     }
-    
+
     render() {
         return (
             <Formik
                 enableReinitialize={true}
-                initialValues={{name: '', email: '', message: ''}}
-    
+                initialValues={{ name: '', email: '', message: '' }}
+
                 validationSchema={Yup.object({
                     name: Yup.string()
                         .required('Required'),
                     email: Yup.string()
                         .required('Required')
-                        .matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, {message: 'Please enter a valid Email'}),
+                        .matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, { message: 'Please enter a valid Email' }),
                     message: Yup.string()
                         .required('Required')
                 })}
                 onSubmit={this.handleSubmit}
             >
-            {formik => (
-            <form onSubmit={formik.handleSubmit}>
-                <div style={{marginRight: '1rem'}}>
-                    <StyledLabel>
-                        Name <span style={{color: 'red'}}>{formik.touched.name && formik.errors.name ? '*' + formik.errors.name : null}</span>
-                        <StyledInput width='20' id='name' error={formik.touched.name && formik.errors.name} {...formik.getFieldProps('name')} />
-                    </StyledLabel>
-                    <StyledLabel>
-                        E-mail <span style={{color: 'red'}}>{formik.touched.email && formik.errors.email ? '*' + formik.errors.email : null}</span>
-                        <StyledInput width='20' id='email' error={formik.touched.email && formik.errors.email} {...formik.getFieldProps('email')} />
-                    </StyledLabel>
-                    <StyledLabel>
-                        Message <span style={{color: 'red'}}>{formik.touched.message && formik.errors.message ? '*' + formik.errors.message : null}</span>
-                        <StyledTextarea id='message' error={formik.touched.message && formik.errors.message} {...formik.getFieldProps('message')} />
-                    </StyledLabel>
-                    <LoadingButton done={this.props.done} formStatus={this.state.formStatus} type='submit' error={Object.entries(formik.errors).length !== 0 || this.props.error} loading={formik.isSubmitting || formik.isValidating}>{this.state.formStatus}</LoadingButton>
-                </div>
-            </form>
-            )}
+                {formik => (
+                    <form onSubmit={formik.handleSubmit}>
+                        <div style={{ marginRight: '1rem' }}>
+                            <StyledLabel>
+                                Name <span style={{ color: 'red' }}>{formik.touched.name && formik.errors.name ? '*' + formik.errors.name : null}</span>
+                                <StyledInput width='20' id='name' error={formik.touched.name && formik.errors.name} {...formik.getFieldProps('name')} />
+                            </StyledLabel>
+                            <StyledLabel>
+                                E-mail <span style={{ color: 'red' }}>{formik.touched.email && formik.errors.email ? '*' + formik.errors.email : null}</span>
+                                <StyledInput width='20' id='email' error={formik.touched.email && formik.errors.email} {...formik.getFieldProps('email')} />
+                            </StyledLabel>
+                            <StyledLabel>
+                                Message <span style={{ color: 'red' }}>{formik.touched.message && formik.errors.message ? '*' + formik.errors.message : null}</span>
+                                <StyledTextarea id='message' error={formik.touched.message && formik.errors.message} {...formik.getFieldProps('message')} />
+                            </StyledLabel>
+                            <LoadingButton done={this.props.done} formStatus={this.state.formStatus} type='submit' error={Object.entries(formik.errors).length !== 0 || this.props.error} loading={formik.isSubmitting || formik.isValidating}>{this.state.formStatus}</LoadingButton>
+                        </div>
+                    </form>
+                )}
             </Formik>
         )
     }
